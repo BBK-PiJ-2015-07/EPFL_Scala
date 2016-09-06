4bd7891264a6bb1fa8c75d08e889df171267f560
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2016-09-06 23:28:51 +0100

    IntelliJ Test commit

--- Week1/recfun/src/main/scala/recfun/Main.scala
+++ Week1/recfun/src/main/scala/recfun/Main.scala
@@ -13,7 +13,13 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(c: Int, r: Int): Int = {
+      if(r < 0 || c < 0) 0
+      if(c == 0 || c == r) 1
+      else {
+        pascal(c - 1, r - 1) + pascal(c, r - 1)
+      }
+  }
   
   /**
    * Exercise 2
