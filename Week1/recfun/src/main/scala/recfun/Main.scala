3192eca00be10ca13c02417c4734c8dc554dfa85
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2016-09-06 23:55:25 +0100

    Week1 - Pascal's triangle function done

--- Week1/recfun/src/main/scala/recfun/Main.scala
+++ Week1/recfun/src/main/scala/recfun/Main.scala
@@ -16,9 +16,7 @@ object Main {
     def pascal(c: Int, r: Int): Int = {
       if(r < 0 || c < 0) 0
       if(c == 0 || c == r) 1
-      else {
-        pascal(c - 1, r - 1) + pascal(c, r - 1)
-      }
+      else { pascal(c - 1, r - 1) + pascal(c, r - 1) }
   }
   
   /**
