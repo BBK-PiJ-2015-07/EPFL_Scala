20b7a9288df8600ca4f99b854854e4f6b76c89b4
devacc <devaccpub@gmail.com>
2016-10-11 17:33:22 +0100

    Week5 - wordOccurrences done

--- Week5/forcomp/src/main/scala/forcomp/Anagrams.scala
+++ Week5/forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -34,7 +34,7 @@ object Anagrams {
    *
    *  Note: you must use `groupBy` to implement this method!
    */
-  def wordOccurrences(w: Word): Occurrences = ???
+  def wordOccurrences(w: Word): Occurrences = w.toLowerCase().groupBy(identity).map{ p => (p._1, p._2.length)}.toList sortBy(_._1)
 
   /** Converts a sentence into its character occurrence list. */
   def sentenceOccurrences(s: Sentence): Occurrences = ???
